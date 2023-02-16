c020c0b7f386ef5c21f7ea037f4abbdfb92174d2
poojajadhav72301 <poojajadhav72301@gmail.com>
2023-02-16 08:46:44 +0530

    update helloWorld file
    update helloWorld file

--- helloWorld.js
+++ helloWorld.js
@@ -1,2 +1,14 @@
 console.log("Pooja R Jadhav");
 console.log("Hello World");
+console.log(24);
+
+var fullName = "Pooja R Jadhav"; //variable declartion and  intalization
+console.log(fullName);
+
+var city;  //variable declaration 
+console.log(city);
+city = "Kundal"; // variable intalization
+console.log(city);
+
+city = "Karad"; // variable update
+console.log(city);
