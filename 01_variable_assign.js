9180840385d07bd0ffdb28564a326cefd41556e5
poojajadhav72301 <poojajadhav72301@gmail.com>
2023-02-16 14:21:09 +0530

    create 01_variable_assign file
    create 01_variable_assign file

---
+++ 01_variable_assign.js
@@ -0,0 +1,24 @@
+console.log(" ****** step 1 ****** ")
+
+var myFullName;
+console.log(myFullName);
+myFullName = "Pooja Jadhav";
+console.log("First and last name:", myFullName);
+myFullName = "Pooja Rajendra Jadhav";
+console.log("Full name:", myFullName);
+
+console.log(" ****** step 2 ****** ");
+
+var myLove = "I love only JS";
+console.log(myLove);
+
+console.log(" ****** step 3 ****** ");
+var firstName = "Pooja", lastName = "Jadhav", age = 24, address = "Sangli", pincode = 416309, gender = "Female", isMarried ="No";
+console.log("First name:", firstName);
+console.log("Last name:", lastName);
+console.log("Age:", age);
+console.log("Address:", address);
+console.log("Pincode:", pincode);
+console.log("Gender:", gender);
+console.log("Married:", isMarried);
+
