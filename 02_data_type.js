fc6056840b6c6685c782cbf65dcd504f70954747
poojajadhav72301 <poojajadhav72301@gmail.com>
2023-02-17 08:56:33 +0530

    create 02_data_type.js file
    create 02_data_type.js file

---
+++ 02_data_type.js
@@ -0,0 +1,28 @@
+/*var name = "Mohit Sharma";
+
+var age = 24;
+var weight = 50;
+
+var isMarried = false;
+
+console.log("type of name: ", typeof name);
+console.log("type of age and weight:", typeof age);
+console.log("type of married:", typeof isMarried);
+
+var graduation;
+console.log("graduation: ", graduation);
+console.log("type of address", typeof graduation);
+
+var city = null;
+console.log("city: ", city);
+console.log("type of city", typeof city);*/
+
+
+var num1 = 100;
+var num2 = 200;
+
+var temp = num1;
+
+num1 = num2;
+num2 = temp;
+console.log(num1, num2);
