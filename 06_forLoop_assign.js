81d02d7a555a729a6de15a0caa133ad37638842b
poojajadhav72301 <poojajadhav72301@gmail.com>
2023-03-01 11:31:32 +0530

    create 06_forLoop_assign file
    create 06_forLoop_assign file

---
+++ 06_forLoop_assign.js
@@ -0,0 +1,41 @@
+// for loop
+console.log("** WAP to print no from 5 to 15 by incrementing 1 **");
+for(let num = 5; num <= 15; num++){
+    console.log(num);
+}
+
+console.log(" ");
+console.log("** WAP to print no from 50 to 40 by decrementing 1 **");
+for(let num = 50; num >=40; num--){
+    console.log(num);
+}
+
+console.log(" ");
+console.log("** WAP to find first 15 odd no **");
+for(let num = 1; num <= 20; num = num + 2){
+    console.log(num);
+}
+
+console.log(" ");
+console.log("** WAP to find first 10 even no **");
+for(let num = 0; num <= 18; num = num + 2){
+    console.log(num);
+}
+
+console.log(" ");
+console.log("** WAP to print a table of 5 **");
+for(let num = 5; num <= 50; num = num + 5){
+    console.log(num);
+}
+
+console.log(" ");
+console.log("** WAP to print a table of 10 **");
+for(let num = 10; num <= 100; num = num + 10){
+    console.log(num);
+}
+
+console.log(" ");
+console.log("** WAP to print a table of 10 in reverse order **");
+for(let num = 100; num >= 10; num = num - 10){
+    console.log(num);
+}
