8afea1251d7057253a2c0a2e5abee28b2fd4706e
poojajadhav72301 <poojajadhav72301@gmail.com>
2023-03-01 11:05:49 +0530

    create 05_incrementDecrement  and  06_for_loop file
    create 05_incrementDecrement  and
    06_for_loop file

---
+++ 05_incrementDecrement.js
@@ -0,0 +1,11 @@
+console.log("***** post increment *****");
+var index = 10;
+var i = index++;// index = index -1
+console.log(`index: ${index} , i: ${i}`);
+
+console.log(" ");
+console.log("***** pre increment *****");
+var index = 5;
+var i = ++index;// index = index -1
+console.log(`index: ${index} , i: ${i}`);
+
---
+++ 06_for_loop.js
@@ -0,0 +1,18 @@
+// for loop
+console.log("***** print no 1 to 10 using for loop *****");
+for(let i = 1; i <= 10; i++){
+    console.log(i);
+}
+
+console.log(" ");
+console.log("***** print no 10 to 1 using for loop *****");
+for(i = 10; i>= 0; i--){
+    console.log(i);
+}
+
+console.log(" ");
+console.log("***** table of 5 *****");
+for(i = 5; i <= 50; i = i + 5){
+    console.log(i);
+}
+
