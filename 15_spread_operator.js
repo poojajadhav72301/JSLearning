76546381fe869849154947c6a26fda3fb03f41ed
poojajadhav72301 <poojajadhav72301@gmail.com>
2023-03-17 09:02:09 +0530

    updated prime_number and created  callBank, 14_higher_order, 15_spread_operator
    updated prime_number and created
    callBank, 14_higher_order, 15_spread_operator

---
+++ 15_spread_operator.js
@@ -0,0 +1,7 @@
+
+// ... Rest Parameter
+function show(arg1, ...arg){
+    console.log(arg1, arg);
+
+}
+show(3, 4, 6, 8, 9);
--- prime_number.js
+++ prime_number.js
@@ -13,3 +13,32 @@ for(let i = 2; i < 30; i++){
         console.log(i);
     }
 }
+
+
+console.log(" ");
+function primeNumbers(startValue,totalPrime){
+    let count = 0;
+    let num = startValue;
+    const arrayOfPrime = [];
+    while (count<totalPrime) {
+        let isPrimeNum = isPrime(num);
+        if(isPrimeNum) {
+            count++;
+            arrayOfPrime.push(num);
+        }
+        num++;
+    } 
+    console.log(arrayOfPrime); 
+}
+
+function isPrime(num){
+    for (let index = 2; index < num; index++) { // num = 7 index= 7
+        if(num%index ==0) {// 7%6==0
+            return false;
+        }
+    }
+    return true;
+}
+primeNumbers(3, 10);
+primeNumbers(20, 10);
+primeNumbers(50, 15);
