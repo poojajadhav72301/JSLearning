8acdb42527df57936141d3090fe3335d574afbfb
poojajadhav72301 <poojajadhav72301@gmail.com>
2023-03-21 08:47:34 +0530

    created 15_forEachArray and 15_forEachArrayObject file
    created 15_forEachArray and 15_forEachArrayObject file

---
+++ 15_forEachArray.js
@@ -0,0 +1,32 @@
+const array = [2, 3, 4, 5, 9];
+array.forEach( function( currentValue, index, array ) {
+    console.log(currentValue, index, array);
+});
+
+array.forEach( ( currentValue ) => {
+    console.log(currentValue);
+});
+
+console.log("even num");
+array.forEach(element => {
+    if(element % 2 == 0){
+        console.log(element);
+    }
+});
+
+console.log("Sum the array.....");
+let sum = 0;
+array.forEach( (element) => {
+    sum = sum + element;
+});
+console.log(sum);
+
+console.log("Even index element in the array");
+const newArray = [];
+array.forEach((element, index) => {
+    if (index%2==0) {
+        //console.log(element);
+        newArray.push(element);
+    }
+});
+console.log(newArray);
